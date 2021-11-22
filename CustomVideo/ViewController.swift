8a3a42480f8196b3ceae10c4b9f55209fe4877b1
Chad Parker <cparker@rzsoftware.com>
2021-11-22 07:41:14 -0800

    Initial Commit

---
+++ CustomVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomVideo
+//
+//  Created by Chad Parker on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
